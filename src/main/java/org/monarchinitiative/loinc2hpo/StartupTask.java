114285c56f90edb3e128b5536ac3c09e6608bbe8
pnrobinson <peter.robinson@jax.org>
2021-11-03 22:24:33 -0400

    logging

--- src/main/java/org/monarchinitiative/loinc2hpo/StartupTask.java
+++ src/main/java/org/monarchinitiative/loinc2hpo/StartupTask.java
@@ -3,6 +3,7 @@ package org.monarchinitiative.loinc2hpo;
 import javafx.concurrent.Task;
 import org.monarchinitiative.loinc2hpo.except.Loinc2HpoRunTimeException;
 import org.monarchinitiative.loinc2hpo.io.JsonHpoParser;
+import org.monarchinitiative.loinc2hpocore.io.LoincTableCoreParser;
 import org.monarchinitiative.loinc2hpocore.loinc.LoincEntry;
 import org.monarchinitiative.loinc2hpocore.loinc.LoincId;
 import org.monarchinitiative.phenol.ontology.data.Ontology;
@@ -61,38 +62,16 @@ public final class StartupTask extends Task<Void> {
         LOGGER.info("Starting Startup task");
         ingestOntology();
         ingestLoincTable();
+        LOGGER.info("Finishing Startup task");
         return null;
     }
 
 
     private void ingestLoincTable() {
         String loincTablePath = optionalResources.getLoincCoreTablePath();
-        Map<LoincId, LoincEntry> loincMap = new HashMap<>();
-        int count_malformed = 0;
-        int count_correct = 0;
-        try (BufferedReader br = new BufferedReader(new FileReader(loincTablePath))){
-            String line = br.readLine(); // first line is header
-            if (! line.contains("\"LOINC_NUM\"")) {
-                LOGGER.error(String.format("Malformed header line (%s) in Loinc File %s",line,loincTablePath));
-                optionalResources.setLoincTableMap(Map.of());
-            }
-            while ((line=br.readLine())!=null) {
-                try {
-                    LoincEntry entry = LoincEntry.fromQuotedCsvLine(line);
-                    loincMap.put(entry.getLoincId(),entry);
-                    count_correct++;
-                } catch (Loinc2HpoRunTimeException e) {
-                    LOGGER.error("Malformed loinc code in the line:\n " + line);
-                    count_malformed++;
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        LOGGER.info(count_correct+ " loinc entries were ingested");
-        if (count_malformed > 0) {
-            LOGGER.warn(count_malformed + " loinc numbers (identifiers) were malformed");
-        }
+        LOGGER.info("Loading LOINC from {}", loincTablePath);
+        LoincTableCoreParser coreParser = new LoincTableCoreParser(loincTablePath);
+        Map<LoincId, LoincEntry> loincMap = coreParser.getLoincEntries();
         // set to an immutable map
         optionalResources.setLoincTableMap(Map.copyOf(loincMap));
     }
@@ -114,6 +93,7 @@ public final class StartupTask extends Task<Void> {
                     final Ontology ontology = JsonHpoParser.loadOntology(ontologyPath);
                     optionalResources.setOntology(ontology);
                     updateMessage("Ontology loaded");
+                    LOGGER.info("Ontology loaded");
                 } catch (Exception e) {
                     updateMessage(String.format("Error loading HPO file : %s", e.getMessage()));
                     LOGGER.warn("Error loading HPO file: ", e);
